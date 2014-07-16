9089b48d1bbe4a043636fa8c0d4f8ef2499f34dc
kokoreko <kosta.lazarev@gmail.com>
2014-07-16 20:03:15 +0300

    Start first build of project added: Program.java war.xml

---
+++ src/Program.java
@@ -0,0 +1,20 @@
+/*
+ * WarGames main class
+ */
+
+
+/**
+ * 
+ * Main program class that lunches the system into activity
+ * 
+ * @author by Kosta Lazarev
+ * @version 16/07/2014
+ */
+public class Program {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
